8b9f1377916422b8d255bfc3aff9ecee83c27cae
berkott <berkott@gmail.com>
2019-04-02 21:17:54 -0400

    adding Sleep Apnea, destination titan

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	$('#pagepiling').pagepiling({
-        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7'],
-        sectionsColor: ['#ce983a', '#7b572f', '#485013', '#253c87', '#a22729', '#102877', '#111111'],
-        navigation: {
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9'],
+        sectionsColor: ['#ce983a', '#71af0f', '#1b3596', '#7b572f', '#485013', '#253c87', '#a22729', '#102877', '#111111'],
+        navigation: { //#7aa1ff
             'position': 'right',
-            'tooltips': ['Home', 'Mole Project', 'Space Invaders', 'SmartLib', 'Titan Power Up', 'Techno Titans', 'The Game Palace']
+            'tooltips': ['Home', 'Sleep Apnea AI','Destination: Titan', 'Mole Project', 'Space Invaders', 'SmartLib', 'Titan Power Up', 'Techno Titans', 'The Game Palace']
         },
         afterRender: function(){
             $('#pp-nav').addClass('custom');
