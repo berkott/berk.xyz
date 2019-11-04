e6368ef07df582689c35090b740342655ed39d75
berkott <berkott@gmail.com>
2019-11-03 20:51:27 -0500

    november 2019 updates

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
 	$('#pagepiling').pagepiling({
-        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9'],
-        sectionsColor: ['#ce983a', '#71af0f', '#1b3596', '#7b572f', '#485013', '#253c87', '#a22729', '#102877', '#111111'],
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9',
+            'page10', 'page11', 'page12', 'page13'],
+        sectionsColor: ['#ce983a', '#8a8a8a', '#102877', '#0f1745', '#0b3b79', '#71af0f', '#1b3596',
+            '#7b572f', '#485013', '#253c87', '#a22729', '#102877', '#111111'],
         navigation: { //#7aa1ff
             'position': 'right',
-            'tooltips': ['Home', 'Sleep Apnea AI','Destination: Titan', 'Mole Project', 'Space Invaders', 'SmartLib', 'Titan Power Up', 'Techno Titans', 'The Game Palace']
+            'tooltips': ['Home', 'Blind Assistant', 'Techno Titans VP', 'Programming Club VP', 'Senslytics Intern',
+                'Sleep Apnea AI','Destination: Titan', 'Mole Project', 'Space Invaders', 'SmartLib', 'Titan Power Up',
+                'Techno Titans', 'The Game Palace']
         },
+
         afterRender: function(){
             $('#pp-nav').addClass('custom');
         },
