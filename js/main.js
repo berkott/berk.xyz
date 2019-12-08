4569e24478fdc20cb85e7d617ffe2ae91be7f33c
berkott <berkott@gmail.com>
2019-12-08 15:48:02 -0500

    december 2019 updates

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	$('#pagepiling').pagepiling({
         anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9',
             'page10', 'page11', 'page12', 'page13'],
-        sectionsColor: ['#ce983a', '#8a8a8a', '#102877', '#0f1745', '#0b3b79', '#71af0f', '#1b3596',
-            '#7b572f', '#485013', '#253c87', '#a22729', '#102877', '#111111'],
+        sectionsColor: ['#314776', '#42394d', '#102877', '#0f1745', '#19356c', '#327250', '#1b3596',
+            '#7b572f', '#485013', '#2A4396', '#a22729', '#102877', '#111111'],
         navigation: { //#7aa1ff
             'position': 'right',
             'tooltips': ['Home', 'Blind Assistant', 'Techno Titans VP', 'Programming Club VP', 'Senslytics Intern',
